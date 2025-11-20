dd9f9122ddf1e55a91464ab49de71c5c6fa907f1
Vitalii Koretskyi <wondertalik@gmail.com>
2025-11-20 05:10:55 +0000

    Implement serializers, views, admin, tests, and API routing for all collections

--- octofit-tracker/backend/core/admin.py
+++ octofit-tracker/backend/core/admin.py
@@ -1,5 +1,10 @@
 
 from django.contrib import admin
-from .models import Example
+from .models import Example, AppUser, Team, Activity, Workout, Leaderboard
 
 admin.site.register(Example)
+admin.site.register(AppUser)
+admin.site.register(Team)
+admin.site.register(Activity)
+admin.site.register(Workout)
+admin.site.register(Leaderboard)
--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -13,9 +13,35 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from core import views
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+router = routers.DefaultRouter()
+router.register(r'users', views.AppUserViewSet)
+router.register(r'teams', views.TeamViewSet)
+router.register(r'activities', views.ActivityViewSet)
+router.register(r'workouts', views.WorkoutViewSet)
+router.register(r'leaderboard', views.LeaderboardViewSet)
+router.register(r'examples', views.ExampleViewSet)
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': request.build_absolute_uri('users/'),
+        'teams': request.build_absolute_uri('teams/'),
+        'activities': request.build_absolute_uri('activities/'),
+        'workouts': request.build_absolute_uri('workouts/'),
+        'leaderboard': request.build_absolute_uri('leaderboard/'),
+        'examples': request.build_absolute_uri('examples/'),
+    })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('', api_root, name='api_root'),
 ]
